383ddc4c246e43ae396b42e22e6236fd972be55c
Martin Pozor <m.pozor@pozyx.com>
2016-12-20 20:07:41 +0100

    problem 2 solved

---
+++ Pozyx.Euler/Pozyx.Euler.Windows.Console/Problems/2.fs
@@ -0,0 +1,10 @@
+﻿module _2
+
+// https://projecteuler.net/problem=2
+
+let upperBound = 4000000
+let fibSeq = Seq.unfold (fun state -> 
+    Some(fst state + snd state, (snd state, fst state + snd state))) (0, 1)
+let ofValueBelow x seq = seq |> Seq.takeWhile (fun n -> n <= x)
+let even seq = seq |> Seq.filter(fun n -> n % 2 = 0)
+let sum = fibSeq |> ofValueBelow upperBound |> even |> Seq.sum
