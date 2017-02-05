cd38375e3e26c8a64975b37318707df98857de6d
Martin Pozor <m.pozor@pozyx.com>
2017-02-05 21:50:53 +0000

    problem 3

---
+++ Pozyx.Euler/Pozyx.Euler.Windows.Console/Problems/3.fs
@@ -0,0 +1,13 @@
+﻿module _3
+
+// https://projecteuler.net/problem=3
+
+let rec factorize n = 
+    {2L..n} 
+    |> Seq.filter (fun x -> n % x = 0L) 
+    |> Seq.head
+    |> (fun x -> 
+        Seq.singleton x 
+        |> Seq.append (if x = n then Seq.empty else factorize (n / x)))
+
+let largestPrimeFactor = factorize 600851475143L |> Seq.head
