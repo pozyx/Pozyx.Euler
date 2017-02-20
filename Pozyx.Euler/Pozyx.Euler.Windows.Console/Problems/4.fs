66332af3daa53ba3227f38e2c15789c8fe0410f2
Martin Pozor <martin.pozor@pozyx.com>
2017-02-20 23:26:09 +0000

    problem 4 done

---
+++ Pozyx.Euler/Pozyx.Euler.Windows.Console/Problems/4.fs
@@ -0,0 +1,31 @@
+﻿module _4
+
+// https://projecteuler.net/problem=4
+
+let digits = 3
+
+let decompose =
+    Seq.unfold 
+        (fun number ->
+            if number = 0 then None
+            else Some(number % 10, number / 10))
+
+let isPalindrome number =
+    not (Seq.zip (decompose number) (decompose number |> Seq.rev)
+        |> Seq.exists (fun (x, y) -> x <> y))
+
+let minproduct = pown 10 (digits - 1)
+let maxproduct = (pown 10 digits) - 1
+
+let cartesianProducts = 
+    {minproduct..maxproduct}
+    |> Seq.map
+        (fun x -> 
+            {x..maxproduct}
+            |> Seq.map (fun y -> (x, y)))
+    |> Seq.collect (fun x -> x)
+
+let results = cartesianProducts |> Seq.map (fun (p1, p2) -> p1 * p2)
+let palindromes = results |> Seq.filter isPalindrome
+let maxPalindrome = palindromes |> Seq.max
+ 
