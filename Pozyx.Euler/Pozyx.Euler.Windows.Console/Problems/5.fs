687b4a1533c216e6b0a8fb5307e5eabbbae962df
Martin Pozor <m.pozor@pozyx.com>
2017-03-14 21:28:26 +0000

    problem 5 solved

---
+++ Pozyx.Euler/Pozyx.Euler.Windows.Console/Problems/5.fs
@@ -0,0 +1,16 @@
+﻿module _5
+
+// https://projecteuler.net/problem=5
+
+let maxDivisor = 20
+
+let divisors = {2..maxDivisor}
+
+let rec gcd (a, b) =
+    match (a, b) with
+    | (a, 0) -> a
+    | (a, b) -> gcd (b, a % b)
+
+let lcm (a, b) = (a / gcd(a,b)) * b
+
+let result = Seq.fold (fun acc next -> lcm(acc, next)) 1 divisors
