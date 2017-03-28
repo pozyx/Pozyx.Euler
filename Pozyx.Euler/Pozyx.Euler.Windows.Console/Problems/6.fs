43d600a98d2b865d4ba606b34fd8f4debd7c78b3
Martin Pozor <m.pozor@pozyx.com>
2017-03-28 22:16:46 +0100

    problem 6 done

---
+++ Pozyx.Euler/Pozyx.Euler.Windows.Console/Problems/6.fs
@@ -0,0 +1,11 @@
+﻿module _6
+
+// https://projecteuler.net/problem=6
+
+let n = 100
+
+let numbers = {1..n}
+
+let result = 
+    (pown (numbers |> Seq.sum) 2)
+    - (numbers |> Seq.map (fun x -> pown x 2) |> Seq.sum)
